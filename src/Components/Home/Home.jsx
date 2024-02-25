37c60c0f5fb0a9f3018db152d457be2021733000
Saurabh <saurabh100rab002@gmail.com>
2024-02-26 00:35:10 +0530

    added the home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,3 @@
-import { Container, Row, Col } from "react-bootstrap";
 import Navbar from "../Navbar/Navbar";
 import "./Home.css";
 
@@ -6,15 +5,9 @@ function Home() {
   return (
     <>
       <Navbar />
-      <Container>
-        <Row>
-          <Col>
-            <div className="bg-container">
-              <h1> Find a job which suits you</h1>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+      <div className="bg-containers">
+        <h1 className="main-heading"> Find a job which suits you</h1>
+      </div>
     </>
   );
 }
