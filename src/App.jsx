069b9967251edb0a8564984051446aed164e7411
Saurabh <saurabh100rab002@gmail.com>
2024-02-25 18:49:54 +0530

    added the folders

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1> Job-Quilt </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,11 @@
+import "./Login.css";
+
+function Login() {
+  return (
+    <div>
+      <h2> This is login page</h2>
+    </div>
+  );
+}
+
+export default Login;
