4836a37156edc757c696f50a9d2727bf1f46193f
Saurabh <saurabh100rab002@gmail.com>
2024-03-11 20:10:03 +0530

    added style in job-detail-page

--- src/Components/Jobdetailspage/Jobdetailspage.jsx
+++ src/Components/Jobdetailspage/Jobdetailspage.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import "./Jobdetailspage.css";
 import { useParams } from "react-router-dom";
 import Cookies from "js-cookie";
+import { CiLocationOn } from "react-icons/ci";
+import { MdWorkOutline } from "react-icons/md";
+import Navbar from "../Navbar/Navbar";
 
 function Jobdetailspage() {
   const [jobDetails, setJobDetails] = useState({});
@@ -34,45 +37,69 @@ function Jobdetailspage() {
   }, [id]);
 
   return (
-    <div>
-      <div>
-        <p className="mmmssp">{jobDetails.job_description}</p>
-        <img src={jobDetails.company_logo_url} alt="Company Logo" />
-        <p>{jobDetails.employment_type}</p>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href={jobDetails.company_website_url}
-        >
-          Apply Now
-        </a>
-        <p>{jobDetails.location}</p>
-        <p>{jobDetails.package_per_annum}</p>
-        <h2>{jobDetails.title}</h2>
+    <>
+      <Navbar />
+      <div className="conatines">
+        <h3>{jobDetails.title}</h3>
+        <div className="image-rating-con">
+          <img
+            src={jobDetails.company_logo_url}
+            alt="company-url"
+            className="company-img"
+          />
+          <p>{`⭐`.repeat(jobDetails.rating)}</p>
+        </div>
+        <div className="location-job-pacakge-container">
+          <div className="location-map-container">
+            <CiLocationOn />
+            <p className="locations">{jobDetails.location}</p>
+          </div>
+          <div className="work-jobtype">
+            <MdWorkOutline />
+            <p className="job-type">{jobDetails.employment_type}</p>
+          </div>
+          <p className="package"> {jobDetails.package_per_annum}</p>
+        </div>
+        <hr className="horizontal-line" />
+        <div className="d-flex justify-content-between">
+          <h5> Job Description: </h5>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={jobDetails.company_website_url}
+            className="apply_now"
+          >
+            Apply_Here
+          </a>
+        </div>
+        <p className="job-description">{jobDetails.job_description}</p>
+
+        {/* here the code is bit changed  */}
+        <div>
+          <h3>Skills</h3>
+          {jobDetails.skills &&
+            jobDetails.skills.map((skill, index) => (
+              <div key={index} className="d-flex">
+                <img src={skill.image_url} alt={skill.name} />
+                <p>{skill.name}</p>
+              </div>
+            ))}
+        </div>
+        <div>
+          <h3>Life at Company</h3>
+          <p>
+            {jobDetails.life_at_company &&
+              jobDetails.life_at_company.description}
+          </p>
+          <img
+            src={
+              jobDetails.life_at_company && jobDetails.life_at_company.image_url
+            }
+            alt="Life at Company"
+          />
+        </div>
       </div>
-      <div>
-        <h3>Life at Company</h3>
-        <p>
-          {jobDetails.life_at_company && jobDetails.life_at_company.description}
-        </p>
-        <img
-          src={
-            jobDetails.life_at_company && jobDetails.life_at_company.image_url
-          }
-          alt="Life at Company"
-        />
-      </div>
-      <div>
-        <h3>Skills</h3>
-        {jobDetails.skills &&
-          jobDetails.skills.map((skill, index) => (
-            <div key={index} className="d-flex">
-              <img src={skill.image_url} alt={skill.name} />
-              <p>{skill.name}</p>
-            </div>
-          ))}
-      </div>
-    </div>
+    </>
   );
 }
 
